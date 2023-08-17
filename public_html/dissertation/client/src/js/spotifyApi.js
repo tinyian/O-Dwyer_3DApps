c18f1ee3b5160cbd6f3711e9a11df43671ecfff2
tinyian <81102958+tinyian@users.noreply.github.com>
2023-08-17 17:21:46 +0100

    Fixed

--- public_html/dissertation/client/src/js/spotifyApi.js
+++ public_html/dissertation/client/src/js/spotifyApi.js
@@ -1,11 +1,7 @@
-var client_id = "650c310f0b86447a986d654cad6f4053";
+var redirect_uri = "http://users.sussex.ac.uk/~io202/dissertation/client/public/diss.html";
+
+var client_id = "";
 var client_secret = "";
-var redirect_uri = "http://users.sussex.ac.uk/~io202/dissertation/client/public/index.html";
-
-let codeVerifier = generateRandomString(128);
-const urlParams = new URLSearchParams(window.location.search);
-let code = urlParams.get('code');
-
 
 var access_token = null;
 var refresh_token = null;
@@ -66,97 +62,6 @@ function onPageLoad(){
         }
     }
 }
-
-//login test tings
-
-function generateRandomString(length) {
-  let text = '';
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-async function generateCodeChallenge(codeVerifier) {
-  function base64encode(string) {
-    return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
-  }
-
-  const encoder = new TextEncoder();
-  const data = encoder.encode(codeVerifier);
-  const digest = await window.crypto.subtle.digest('SHA-256', data);
-
-  return base64encode(digest);
-}
-
-generateCodeChallenge(codeVerifier).then(codeChallenge => {
-  let state = generateRandomString(16);
-  let scope = 'user-read-private user-read-email';
-
-  localStorage.setItem('code_verifier', codeVerifier);
-
-  let args = new URLSearchParams({
-    response_type: 'code',
-    client_id: clientId,
-    scope: scope,
-    redirect_uri: redirectUri,
-    state: state,
-    code_challenge_method: 'S256',
-    code_challenge: codeChallenge
-  });
-
-  window.location = 'https://accounts.spotify.com/authorize?' + args;
-});
-
-let codeVerifier = localStorage.getItem('code_verifier');
-
-let body = new URLSearchParams({
-  grant_type: 'authorization_code',
-  code: code,
-  redirect_uri: redirectUri,
-  client_id: clientId,
-  code_verifier: codeVerifier
-});
-
-const response = fetch('https://accounts.spotify.com/api/token', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: body
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('HTTP status ' + response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    localStorage.setItem('access_token', data.access_token);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-async function getProfile(accessToken) {
-  let accessToken = localStorage.getItem('access_token');
-
-  const response = await fetch('https://api.spotify.com/v1/me', {
-    headers: {
-      Authorization: 'Bearer ' + accessToken
-    }
-  });
-
-  const data = await response.json();
-    console.log(data);
-}
-
-//login test tings
 
 function handleRedirect(){
     let code = getCode();
